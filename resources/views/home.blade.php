a4195e3078d6ed16efec6659faf57ab56594b111
MonicaDeBona <debona.monica@outlook.it>
2023-02-09 16:53:33 +0100

    print movies

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,26 +1,34 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.app')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    <!-- Styles -->
-    @vite('resources/js/app.js')
-
-</head>
-
-<body>
-
+@section('main-content')
     <main>
-
+        <div class="container">
+            <div class="row">
+                @foreach ($movies as $movie)
+                    <div class="col-3">
+                        <div class="card my-2 mx-1">
+                            <img src="{{ Vite::asset('resources/assets/img/poster-not-available.png') }}" alt="Movie Poster">
+                            <div class="card-body text-center">
+                                <h4>
+                                    {{ $movie->title }}
+                                </h4>
+                                <h4>
+                                    {{ $movie->original_title }}
+                                </h4>
+                                <p>
+                                    {{ $movie->nationality }}
+                                </p>
+                                <p>
+                                    {{ $movie->date }}
+                                </p>
+                                <p>
+                                    {{ $movie->vote }}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
     </main>
-
-</body>
-
-</html>
+@endsection
